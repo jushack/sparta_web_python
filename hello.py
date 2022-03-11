bfc563efe758cb446b6808ce12d8294c6e191c90
jushack <3.jushack.1@gmail.com>
2022-03-11 10:54:28 +0900

    prac-2

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-import requests  # requests 라이브러리 설치 필요
+import requests
+from bs4 import BeautifulSoup
 
-r = requests.get(
-    'http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(
+    'https://movie.naver.com/movie/sdb/rank/rmovie.naver', headers=headers)
 
-gus = rjson['RealtimeCityAir']['row']
+soup = BeautifulSoup(data.text, 'html.parser')
 
-for gu in gus:
-    gu_name = gu['MSRRGN_NM']
-    gu_mise = gu['IDEX_MVL']
-    if gu_mise > 100:
-        print(gu_name, gu_mise)
+trs = soup.select('#old_content > table > tbody > tr')
+
+for tr in trs:
+    a_tag = tr.select_one('td.title > div > a')
+    if a_tag is not None:
+        title = a_tag.text
+        print(title)
